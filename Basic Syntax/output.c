92eb0cf809862dbde3db53943341a8c9c83652c8
deadmanShadow <raihanshamil33@gmail.com>
2024-03-30 23:43:28 +0600

    out added

---
+++ Basic Syntax/output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int rahim, karim;
+    char K;
+    float f;
+    scanf("%d %c %f", &rahim, &K , &f);
+    printf("%d %c %0.2f", rahim, K, f);
+
+    int l,o;
+    scanf("%d%% %d%%", &l, &o);
+    printf("%d%% %d%%", l,o);
+    return 0;
+}
