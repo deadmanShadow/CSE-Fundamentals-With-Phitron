a94e2af623e88dd9c9f1027ebdd039f4a97df3ff
deadmanShadow <raihanshamil33@gmail.com>
2024-03-30 22:49:30 +0600

    basic added

---
+++ Basic Syntax/print.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("hello\nHi this  is a test off \n");
+    printf("hello this is tab\ttab One opened\n");
+    printf("100%%%%");
+    return 0;
+}
+
+// special characters (ESCAPE)
