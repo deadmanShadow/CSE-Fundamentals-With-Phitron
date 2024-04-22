ca1011572191e9f245e22e05bd6ff279e190cdf8
deadmanShadow <raihanshamil33@gmail.com>
2024-04-22 11:47:52 +0600

    new string

---
+++ A_Create_A_New_String.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[1001], t[1001];
+    scanf("%s %s", &s, &t);
+    int lengS = strlen(s);
+    int lengT = strlen(t);
+    printf("%d %d\n", lengS, lengT);
+    printf("%s %s", s, t);
+    return 0;
+}
