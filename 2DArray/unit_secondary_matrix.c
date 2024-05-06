c7e27bafc5c59003155037366c05c54f90100087
deadmanShadow <raihanshamil33@gmail.com>
2024-05-06 10:40:15 +0600

    units

---
+++ 2DArray/unit_secondary_matrix.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int a[row][col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int k = 0; k < col; k++)
+        {
+            scanf("%d", &a[i][k]);
+        }
+    }
+    int flag = 1;
+    if (row != col)
+    {
+        flag = 0;
+    }
+    for (int i = 0; i < row; i++)
+    {
+        for (int k = 0; k < col; k++)
+        {
+            if (i + k == row - 1)
+            {
+                if (a[i][k] != 1)
+                {
+                    flag = 0;
+                }
+            }
+            else if (a[i][k] != 0)
+            {
+                flag = 0;
+            }
+        }
+    }
+    if (flag == 1)
+    {
+        printf("Secondary Unit Matrix\n");
+    }
+    else
+    {
+        printf("Not Secondary Unit Matrix\n");
+    }
+    return 0;
+}
