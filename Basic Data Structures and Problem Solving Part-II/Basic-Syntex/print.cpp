c85bdb35e0ca7423ce8081ce925552de9fb126dc
deadmanShadow <raihanshamil33@gmail.com>
2024-05-18 22:36:13 +0600

    print

---
+++ Basic Data Structures and Problem Solving Part-II/Basic-Syntex/print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+int main(){
+    std::cout<<"Hello World";
+
+    // data types are almost same
+
+    int a =10;
+    long long int b = 10000;
+    float c = 2.5; 
+    double d = 1.56666;
+    char cc = 'A';
+    std::cout<<"my fav number is " <<a;
+    std::cout<<"my "<<a<<" fav number is";
+    std::cout<<a<<" "<<b;
+    std::cout<<a<<std::endl<<c;
+    return 0;
+}
