2a880d90d58ba8233b2df6ed9f40c022869afd75
deadmanShadow <raihanshamil33@gmail.com>
2024-05-18 23:25:26 +0600

    set

---
+++ Basic Data Structures and Problem Solving Part-II/Basic-Syntex/setprecision.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <iomanip>
+using namespace std;
+int main(){
+    double a;
+    cin>>a;
+    cout<<a<<endl;
+    cout<<fixed<<setprecision(2)<<a<<endl;
+    return 0;
+}
