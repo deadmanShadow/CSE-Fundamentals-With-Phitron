2de0b6c970b4e941208846e7dbedfb0d5b38a06c
deadmanShadow <raihanshamil33@gmail.com>
2024-05-18 22:53:34 +0600

    input add

---
+++ Basic Data Structures and Problem Solving Part-II/Basic-Syntex/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=10;
+    long long int b=(long long int)(a);
+    char c;
+    int ascii = int(c);
+   cin>>c; 
+    // cin>>a>>b>>c;
+    // cout<<a<<" "<<b<<endl;
+    cout<<int(c)<<endl;
+}
