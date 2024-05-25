e265444a2749dffa00db19fa441f36f020efbd39
deadmanShadow <raihanshamil33@gmail.com>
2024-05-25 20:56:58 +0600

    capacity

---
+++ Basic Data Structures and Problem Solving Part-II/Introduction To C++ for DSA/String-CLass/capacity.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // string s= "Hello world";
+    // cout<<s.size()<<endl;
+    // cout<<s.max_size()<<endl;
+    // string s="sadhasodhnasoindsaondsaophasdaoidas";
+    // cout<<s.capacity()<<endl;
+    // string a = "ahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhssasdsa";
+    // cout<<a.capacity()<<endl;
+
+    // string p="Hello";
+    // cout<<p<<endl;
+    // p.clear();
+    // cout<<p<<endl;
+    // string s="Hello";
+    // if (s.empty()==true)  cout<<"Empty"<<endl;
+    // else cout<<"Not empty"<<endl;
+    // return 0;
+
+    string s;
+    cin>>s;
+    // s.resize(5);
+    // cout<<s.size()<<endl;
+    s.resize(5);
+    cout<<s<<endl;
+    s.resize(11, 'X');
+    cout<<s<<endl;
+}
