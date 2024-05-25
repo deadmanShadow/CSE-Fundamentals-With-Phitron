7400fedc6c3a104140be4e5a199692a06bf03d06
deadmanShadow <raihanshamil33@gmail.com>
2024-05-25 20:26:37 +0600

    strng

---
+++ Basic Data Structures and Problem Solving Part-II/Introduction To C++ for DSA/String-CLass/string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // char a[10]="Hello";
+    // cout<<a<<endl;
+    // cout<<strlen(a)<<endl;
+    string s="Hello";
+    string s2= "Hi";
+    if (s==s2)
+    {
+        cout<<"Same"<<endl;
+    }else
+    {
+        cout<<"Not Same"<<endl;
+    }
+    
+    
+    
+    return 0;
+}
