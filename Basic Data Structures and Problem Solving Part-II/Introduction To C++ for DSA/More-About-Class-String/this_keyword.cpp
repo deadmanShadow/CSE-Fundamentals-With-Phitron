32c2d7b59bf01091b4253c6bd55a713a6acc70e2
deadmanShadow <raihanshamil33@gmail.com>
2024-05-26 21:57:46 +0600

    this

---
+++ Basic Data Structures and Problem Solving Part-II/Introduction To C++ for DSA/More-About-Class-String/this_keyword.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Person
+{
+public:
+    string name;
+    int age;
+    Person(string name, int age)
+    {
+        this->name = name;
+        this->age = age;
+    }
+    void hello()
+    {
+        cout << "hello" << endl;
+    }
+};
+int main()
+{
+    Person rakib("rakib ahsan", 24);
+    cout << rakib.name << " " << rakib.age << endl;
+    return 0;
+}
