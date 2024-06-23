16d96c0fcdaef68dc76729b59f163c9417d83dd9
deadmanShadow <raihanshamil33@gmail.com>
2024-06-23 22:56:52 +0600

    head

---
+++ Basic Data Structures and Problem Solving Part-II/Introduction To Data Structures/Singly Linked List Operations/insert_at_head.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        cout << endl
+             << "Inserted at Head" << endl
+             << endl;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    cout << endl
+         << "Inserted at Tail" << endl
+         << endl;
+}
+void print_linked_list(Node *head)
+{
+    cout << endl;
+    cout << "Your Linked List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+
+    cout << endl
+         << endl;
+}
+void insert_at_position(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    cout << endl
+         << endl
+         << "Inserted at Position: " << pos << endl
+         << endl;
+}
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+    cout << endl
+         << "Inserted at Head" << endl
+         << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << "Option 1: Insert at tail" << endl;
+        cout << "Option 2: Print Linked List" << endl;
+        cout << "Option 3: Insert any position" << endl;
+        cout << "Option 4: Insert at head" << endl;
+        cout << "Option 5: Terminate" << endl;
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            cout << "Please Enter Value: ";
+            int v;
+            cin >> v;
+            insert_at_tail(head, v);
+        }
+        else if (op == 2)
+        {
+            print_linked_list(head);
+        }
+        else if (op == 3)
+        {
+            int pos, val;
+            cout << "Enter Position ";
+            cin >> pos;
+            cout << "Enter Value ";
+            cin >> val;
+            if (pos == 0)
+            {
+                insert_at_head(head, val);
+            }
+            else
+            {
+                insert_at_position(head, pos, val);
+            }
+        }
+        else if (op == 4)
+        {
+            int val;
+            cout << "Enter Value: ";
+            cin >> val;
+            insert_at_head(head, val);
+        }
+        else if (op == 5)
+        {
+            break;
+        }
+    }
+    return 0;
+}
