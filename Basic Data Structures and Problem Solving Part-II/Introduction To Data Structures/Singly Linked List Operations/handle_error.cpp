34a2fc812a938c25b0ff106d11ae2bae94b73840
deadmanShadow <raihanshamil33@gmail.com>
2024-06-24 16:41:46 +0600

    error handle

---
+++ Basic Data Structures and Problem Solving Part-II/Introduction To Data Structures/Singly Linked List Operations/handle_error.cpp
@@ -0,0 +1,200 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        cout << endl
+             << "Inserted at Head" << endl
+             << endl;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    cout << endl
+         << "Inserted at Tail" << endl
+         << endl;
+}
+void print_linked_list(Node *head)
+{
+    cout << endl;
+    cout << "Your Linked List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+
+    cout << endl
+         << endl;
+}
+void insert_at_position(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+        if (tmp == NULL)
+        {
+            cout << endl
+                 << "Invalid Index" << endl
+                 << endl;
+            return;
+        }
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    cout << endl
+         << endl
+         << "Inserted at Position: " << pos << endl
+         << endl;
+}
+void insert_at_head(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    newNode->next = head;
+    head = newNode;
+    cout << endl
+         << "Inserted at Head" << endl
+         << endl;
+}
+void delete_from_position(Node *head, int pos)
+{
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+        if (tmp == NULL)
+        {
+            cout << endl
+                 << "Invalid Index" << endl
+                 << endl;
+            return;
+        }
+    }
+    if (tmp->next == NULL)
+    {
+        cout << endl
+             << "Invalid Index" << endl
+             << endl;
+        return;
+    }
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+    cout << endl
+         << "Deleted Position " << pos << endl
+         << endl;
+}
+void delete_head(Node *&head)
+{
+    if (head == NULL)
+    {
+        {
+            cout << endl
+                 << "Head is not available" << endl
+                 << endl;
+            return;
+        }
+    }
+
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    cout << endl
+         << "Deleted Head" << endl
+         << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << "Option 1: Insert at tail" << endl;        // done
+        cout << "Option 2: Print Linked List" << endl;     // done
+        cout << "Option 3: Insert any position" << endl;   // done
+        cout << "Option 4: Insert at head" << endl;        // done
+        cout << "Option 5: Delete from Position " << endl; // done
+        cout << "Option 6: Delete Head " << endl;          // done
+        cout << "Option 7: Terminate" << endl;             // done
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            cout << "Please Enter Value: ";
+            int v;
+            cin >> v;
+            insert_at_tail(head, v);
+        }
+        else if (op == 2)
+        {
+            print_linked_list(head);
+        }
+        else if (op == 3)
+        {
+            int pos, val;
+            cout << "Enter Position ";
+            cin >> pos;
+            cout << "Enter Value ";
+            cin >> val;
+            if (pos == 0)
+            {
+                insert_at_head(head, val);
+            }
+            else
+            {
+                insert_at_position(head, pos, val);
+            }
+        }
+        else if (op == 4)
+        {
+            int val;
+            cout << "Enter Value: ";
+            cin >> val;
+            insert_at_head(head, val);
+        }
+        else if (op == 5)
+        {
+            int pos;
+            cout << "Enter Position: ";
+            cin >> pos;
+            if (pos == 0)
+            {
+                delete_head(head);
+            }
+            else
+            {
+                delete_from_position(head, pos);
+            }
+        }
+        else if (op == 6)
+        {
+            delete_head(head);
+        }
+        else if (op == 7)
+        {
+            break;
+        }
+    }
+    return 0;
+}
