a2e17663f1dd493878efaa83260be841e8b4bd48
deadmanShadow <raihanshamil33@gmail.com>
2024-06-25 21:30:30 +0600

    delete

---
+++ Basic Data Structures and Problem Solving Part-II/Introduction To Data Structures/Singly Linked List Recap/delete.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void print_linked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+void delete_fun(Node *head, int pos)
+{
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *deleteFun = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteFun;
+}
+int size(Node *head)
+{
+    Node *tmp = head;
+    int cnt = 0;
+    while (tmp != NULL)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
+    head->next = a;
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    print_linked_list(head);
+    int pos;
+    cin >> pos;
+    if (pos >= size(head))
+    {
+        cout << "Invalid Index" << endl;
+    }
+    else
+    {
+        delete_fun(head, pos);
+    }
+    print_linked_list(head);
+    return 0;
+}
