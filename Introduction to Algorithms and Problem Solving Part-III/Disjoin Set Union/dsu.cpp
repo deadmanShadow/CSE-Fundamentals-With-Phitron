03c7ebf8de6e216228214c3252944590a78b6419
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-16 00:26:00 +0600

    dsu

---
+++ Introduction to Algorithms and Problem Solving Part-III/Disjoin Set Union/dsu.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 5;
+int par[N];
+void dsu_init(int n) {
+  for (int i = 0; i < n; i++) {
+    par[i] = -1;
+  }
+  par[1] = 3;
+  par[2] = 1;
+}
+int find(int node) {
+  if (par[node] == -1) return node;
+  return find(par[node]);
+}
+int main() {
+  dsu_init(4);
+  cout << find(0) << endl;
+  cout << find(1) << endl;
+  cout << find(2) << endl;
+  cout << find(3) << endl;
+  return 0;
+}
