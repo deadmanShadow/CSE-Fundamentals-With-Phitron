bfa88615c39d0105072d5fa1d18f8dec10a74c0a
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-09-16 12:38:33 +0600

    dsu

--- Introduction to Algorithms and Problem Solving Part-III/Disjoin Set Union/dsu.cpp
+++ Introduction to Algorithms and Problem Solving Part-III/Disjoin Set Union/dsu.cpp
@@ -2,24 +2,42 @@
 using namespace std;
 const int N = 1e5 + 5;
 int par[N];
+int groupd_size[N];
 void dsu_init(int n) {
   for (int i = 0; i < n; i++) {
     par[i] = -1;
+    groupd_size[i] = 1;
   }
-  par[1] = 3;
-  par[2] = 1;
 }
-int find(int node) {
+int dsu_find(int node) {
   if (par[node] == -1) return node;
-  int leader = find(par[node]);
+  int leader = dsu_find(par[node]);
   par[node] = leader;
   return leader;
 }
+void dsu_union(int n1, int n2) {
+  int leaderA = dsu_find(n1);
+  int leaderB = dsu_find(n2);
+  par[leaderA] = leaderB;
+}
+void dsu_union_by_size(int n1, int n2) {
+  int leaderA = dsu_find(n1);
+  int leaderB = dsu_find(n2);
+  if (groupd_size[leaderA] > groupd_size[leaderB]) {
+    par[leaderB] = leaderA;
+    groupd_size[leaderA] += groupd_size[leaderB];
+  } else {
+    par[leaderA] = leaderB;
+    groupd_size[leaderB] += groupd_size[leaderA];
+  }
+}
 int main() {
-  dsu_init(4);
-  cout << find(0) << endl;
-  cout << find(1) << endl;
-  cout << find(2) << endl;
-  cout << find(3) << endl;
+  dsu_init(7);
+  dsu_union_by_size(1, 2);
+  dsu_union_by_size(2, 3);
+  dsu_union_by_size(4, 5);
+  dsu_union_by_size(5, 6);
+  cout << dsu_find(1) << endl;
+  cout << dsu_find(4) << endl;
   return 0;
 }
