5a805beb8728eef2595856f1a5ca5e7bbb71c7b6
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2024-10-03 20:42:53 +0600

    variation

---
+++ Introduction to Algorithms and Problem Solving Part-III/0-1 Knapsack Variations/subset_sum_top_down.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool sub_set_sum(int n, int a[], int s) {
+  if (n == 0) {
+    if (s == 0)
+      return true;
+    else
+      return false;
+  }
+  if (a[n - 1] <= s) {
+    bool op1 = sub_set_sum(n - 1, a, s - a[n - 1]);
+    bool op2 = sub_set_sum(n - 1, a, s);
+    return op1 || op2;
+  } else {
+    return sub_set_sum(n - 1, a, s);
+  }
+};
+int main() {
+  int n;
+  cin >> n;
+  int a[n];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int s;
+  cin >> s;
+  if (sub_set_sum(n, a, s)) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+  return 0;
+}
